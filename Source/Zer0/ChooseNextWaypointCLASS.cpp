a21db3960e89cb49f2f19e218dba1dc347388efc
Sabier <hybridcrayon@yahoo.com>
2019-02-18 00:27:25 -0700

    Added "AIModule" and "GameplayTasks" to Zer0.Build.cs, this will help with 'linker' errors. Had to change naming convention because behaviour tree doesn't play nicely with underscores. I also changed my gitignore slightly so that it ignores everything below .vs/...

---
+++ Source/Zer0/ChooseNextWaypointCLASS.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypointCLASS.h"
+
+EBTNodeResult::Type UChooseNextWaypointCLASS::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Help me!"))
+	return EBTNodeResult::Succeeded;
+}
--- Source/Zer0/Zer0.Build.cs
+++ Source/Zer0/Zer0.Build.cs
@@ -8,6 +8,6 @@ public class Zer0 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule", "GameplayTasks" });
 	}
 }
